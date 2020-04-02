79aedd9b1e706a6f450bdbbe18a8462b6f745ca8
Evandy <andi_evandy@yahoo.co.id>
2020-04-02 21:59:11 +0700

    refactor dan tambah router user

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,8 +1,10 @@
 const route = require('express').Router()
 const movieRoute = require('./movieRoute')
 const google = require('./google')
+const userRouter = require('./user')
 
-route.use('/movies',movieRoute)
-router.use('/google', google)
+route.use('/movies', movieRoute)
+route.use('/google', google)
+route.use('/user', userRouter)
 
 module.exports = route

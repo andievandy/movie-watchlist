54a3a7714f56ffb81aab4610a29142796d20fae0
Budi Gunawan <budiindra.g@gmail.com>
2020-04-03 07:25:37 +0700

    1. membuat tampilan login dan register 2. membuat jquery,ajax untuk login dan register

--- client/main.js
+++ client/main.js
@@ -33,3 +33,62 @@ $("#Logout").click(function () {
         $(".signin2").css('display', 'inline-block');
     }
 })
+
+///login///
+$('#login').submit(function(e){
+    e.preventDefault();
+
+    let email = $('#emailLogin').val()
+    let password = $('#passwordLogin').val()
+
+    const user = {
+        email,
+        password
+    }
+
+    ///AJAX POST Login
+    $.ajax({
+        url:'http://localhost:3000/user/login',
+        type: 'POST',
+        data: user
+    })
+    .done(function(data){
+        localStorage.setItem("token",data)
+        $("#Logout").css('display', 'inline-block');
+        $('#loginRegister').hide()
+    })
+    .fail(function(err){
+        console.log(err)
+        // swal("Error!", err.responseJSON.message, "error");
+    })
+})
+
+///register new user
+$('#register').submit(function(e){
+    e.preventDefault();
+    
+    let name = $('#nameRegister').val()
+    let email = $('#emailRegister').val()
+    let password = $('#passwordRegister').val()
+
+    const user = {
+        name,
+        email,
+        password
+    }
+
+    ///AJAX POST Register
+    $.ajax({
+        url:'http://localhost:3000/user/register',
+        type: 'POST',
+        data: user
+    })
+    .done(function(data){
+        $('#modalRegister').modal('hide')
+        console.log(data)
+    })
+    .fail(function(err){
+        console.log(err)
+        // swal("Error!", err.responseJSON.message, "error");
+    })
+})
